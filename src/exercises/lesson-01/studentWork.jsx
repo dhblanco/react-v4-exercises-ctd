cc4f36ae28005a13c67c350251b28bea86e68e81
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-07-07 11:44:36 -0400

    created variables and added elements to create about me page, added screenshot of rendered page

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,27 @@
 
 export default function StudentWork() {
   //add variables here
+
+  const name = 'Daniela Hernández Blanco';
+  const age = 33;
+  const hobbyList = [
+    { id: 1, title: 'hanging out with friends' },
+    { id: 2, title: 'watching and playing rugby' },
+    { id: 3, title: 'eating and cooking' },
+  ];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me!</h1>
+      <ul>
+        {hobbyList.map((hobby) => (
+          <li key={hobby.id}>{hobby.title}</li>
+        ))}
+      </ul>
+      <p>
+        Hello! My name is {name} and {age} years old. I&apos;m a student from
+        Costa Rica studying programming in North Carolina.
+      </p>
     </div>
   );
 }
