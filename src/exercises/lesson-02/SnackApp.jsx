91471d2cee08da37c21f4a9ab7a190029d708764
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-07-13 21:20:04 -0400

    created various components to export and import

---
+++ src/exercises/lesson-02/SnackApp.jsx
@@ -0,0 +1,15 @@
+import Footer from './SnackFooter';
+import Header from './SnackHeader';
+import SnackList from './SnackList';
+
+const SnackApp = () => {
+  return (
+    <>
+      <Header />
+      <SnackList />
+      <Footer />
+    </>
+  );
+};
+
+export default SnackApp;
---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <marquee>I HOPE YOU HAD FUN!</marquee>
+    </footer>
+  );
+}
